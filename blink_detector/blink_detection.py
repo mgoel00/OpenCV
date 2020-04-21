3483d68cd208c9fe6eaeb6efbf00fe1cdbe14c79
Mayank Goel <goel.mayank.13@gmail.com>
2020-04-21 14:58:34 +0530

    Done Blink Detection

---
+++ blink_detector/blink_detection.py
@@ -0,0 +1,50 @@
+from scipy.spatial import distance as dist
+from imutils import face_utils
+import numpy as np
+import imutils
+import dlib
+import cv2
+
+def aspect_ratio(eye):
+    return ( dist.euclidean(eye[1], eye[5]) + dist.euclidean(eye[2], eye[4]) ) / ( 2 * dist.euclidean(eye[0], eye[3]) )
+
+modelLoc = 'shape_predictor_68_face_landmarks.dat'
+
+EAR_THRESHOLD = 0.28                #It is an experimental value. Might be different for your machine.
+COUNTER = 0
+TOTAL_BLINKS = 0
+print("Loading facial landmark predictor model")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(modelLoc)
+#Extracting the landmarks for both the eyes.
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+cap = cv2.VideoCapture(0)
+while True:
+
+    ret, frame = cap.read()
+    if ret:
+        frame = imutils.resize(frame, width=450)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        rects = detector(gray, 0)
+        for rect in rects:
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
+            leftEye,rightEye = shape[lStart:lEnd],shape[rStart:rEnd]
+            EAR = ( aspect_ratio(leftEye) + aspect_ratio(rightEye) )/2
+            print(EAR)
+            if EAR < EAR_THRESHOLD:
+                print("found less")
+                COUNTER += 1
+            else:
+                if COUNTER >= 3:        #To check that the eye was closed for atleast 3 input frames.
+                    TOTAL_BLINKS += 1
+                COUNTER = 0
+            cv2.putText(frame, "Blinks: {}".format(TOTAL_BLINKS), (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.imshow("Video",frame)
+        if cv2.waitKey(1) == 13:
+            break
+
+cv2.destroyAllWindows()
+cap.release()
